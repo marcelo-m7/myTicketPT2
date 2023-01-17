c092a508a43caef91b10fc46ebfd2d535b5116bd
marcelo-m7 <a79433@ualg.pt>
2023-01-17 15:59:37 +0000

    Update main.py
    Reiterado argumentos para eventos, removido códigos não-funcionais

--- Aplicação Python/main.py
+++ Aplicação Python/main.py
@@ -1,13 +1,7 @@
-from trotinetas import (
-    cria_trotineta,
-    imprime_lista_de_trotinetas,
-    nome_ficheiro_lista_de_trotinetas
-)
-from utilizadores import (
-    cria_novo_utilizador,
-    imprime_lista_de_utilizadores,
-    nome_ficheiro_lista_de_utilizadores
-)
+from eventos import (cria_evento,
+                     imprime_lista_de_eventos,
+                     nome_ficheiro_lista_de_eventos
+                     )
 from io_ficheiros import (
     guarda_em_ficheiro,
     le_de_ficheiro
@@ -15,15 +9,12 @@ from io_ficheiros import (
 from io_terminal import (
     imprime_lista_de_dicionarios
 )
-import time
 
 
 def menu():
     """ main menu da aplicação"""
 
-    lista_de_trotinetas = []
-    lista_de_utilizadores = []
-    lista_de_compras = []
+    lista_de_eventos = []
 
     while True:
         print("""
@@ -40,81 +31,46 @@ def menu():
         *********************************************************************
         """)
 
-        op = input("opcao?").lower()
+        op = input("Opção? ").lower()
 
         if op == "x":
             exit()
-        elif op == "tn":
-            nova_trotineta = cria_trotineta()
-            lista_de_trotinetas.append(nova_trotineta)
-        elif op == "tl":
-            imprime_lista_de_trotinetas(lista_de_trotinetas)
-        elif op == "un":
-            novo_utilizador = cria_novo_utilizador()
-            lista_de_utilizadores.append(novo_utilizador)
-        elif op == "ul":
-            imprime_lista_de_utilizadores(lista_de_utilizadores)
+        elif op == "ne":
+            nova_evento = cria_evento()
+            lista_de_eventos.append(nova_evento)
+        elif op == "le":
+            imprime_lista_de_eventos(lista_de_eventos)
         elif op == "g":
-            guarda_as_listas_em_ficheiros(
-                lista_de_trotinetas, lista_de_utilizadores)
+            guarda_as_listas_em_ficheiros(lista_de_eventos)
         elif op == "c":
-            lista_de_trotinetas, lista_de_utilizadores = carrega_as_listas_dos_ficheiros()
-        elif op == "an":
-            if lista_de_utilizadores and lista_de_trotinetas:
-                id_comprador = pergunta_id(
-                    questao="Qual o id do comprador?", lista=lista_de_utilizadores)
-                id_veiculo = pergunta_id(
-                    questao="Qual o id do veiculo?", lista=lista_de_trotinetas)
-                lista_de_compras.append(
-                    [id_comprador, id_veiculo, time.time()])
-            else:
-                print("Erro: tem de ter utilizadores e trotinetas")
-        elif op == "al":
-            pass
-            # todo
-            # imprime_lista_de_compras()
-
+            lista_de_eventos = carrega_as_listas_dos_ficheiros()
 
 def pergunta_id(questao, lista):
-    """ ... ??to do??
-
-    :param questao:
-    :param lista:
-    :return:
-    """
-
     imprime_lista_de_dicionarios(lista)
     while True:
         idx = int(input(questao))
         if 0 <= idx < len(lista):
             return idx
         else:
-            print(
-                f"id inexistente. Tente de novo. Valores admitidos {0} - {len(lista)}")
+            print(f"id inexistente. Tente de novo. Valores admitidos {0} - {len(lista)}")
 
 
 def carrega_as_listas_dos_ficheiros():
     """ ...todo... """
 
-    lista_de_veiculos = le_de_ficheiro(nome_ficheiro_lista_de_trotinetas)
-    lista_de_utilizadores = le_de_ficheiro(nome_ficheiro_lista_de_utilizadores)
-    return lista_de_veiculos, lista_de_utilizadores
+    lista_de_evento = le_de_ficheiro(nome_ficheiro_lista_de_eventos)
+    return lista_de_evento
 
 
-def guarda_as_listas_em_ficheiros(lista_de_veiculos, lista_de_utilizadores):
+def guarda_as_listas_em_ficheiros(lista_de_evento):
     """ ... todo ....
-
-    :param lista_de_utilizadores:
-    :param lista_de_veiculos:
+    :param lista_de_evento:
     :return:
     """
 
     op = input("Os dados nos ficheiros serão sobrepostos. Continuar (S/n)?")
     if op in ['s', 'S', '']:
-        guarda_em_ficheiro(
-            nome_ficheiro_lista_de_trotinetas, lista_de_veiculos)
-        guarda_em_ficheiro(
-            nome_ficheiro_lista_de_utilizadores, lista_de_utilizadores)
+        guarda_em_ficheiro(nome_ficheiro_lista_de_eventos, lista_de_evento)
     else:
         print("Cancelada.")
 
